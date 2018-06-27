2015e664a1caccd693b7e7dcdaf2b9e3f4300d95
nguyen van cong linh <conglinh@Cong-Linh.local>
2018-06-28 00:52:50 +0700

    Create Sign In and Sign Up View

---
+++ InstagramApp/SignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpViewController.swift
+//  InstagramApp
+//
+//  Created by nguyen van cong linh on 27/06/2018.
+//  Copyright © 2018 nguyen van cong linh. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dismiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
